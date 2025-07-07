e780506e6eb23f56878dd8d245b0e167486011d1
Aryan Shrivastava <aryan.s.github@gmail.com>
2025-07-07 12:21:30 +0530

    made mobile responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,22 +3,56 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import nav_und from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
   const [menu, Setmenu] = useState("home");
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const closeMenu = () => {
+    setMobileOpen(false);
+  };
+
   return (
     <div id="navbar" className='navbar'>
-      <AnchorLink className='anchor-link' offset={15} href='#about'> <img onClick={()=>Setmenu("about")} src={logo} alt="" /></AnchorLink>
-      <ul className='nav-menu'>
-        <li> <AnchorLink className='anchor-link' href='#home'> <p onClick={() => Setmenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={nav_und} alt="" /> : <></>}</li>
-        <li> <AnchorLink className='anchor-link' offset={15} href='#about'> <p onClick={() => Setmenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={nav_und} alt="" /> : <></>}</li>
-        <li> <AnchorLink className='anchor-link' offset={15} href='#services'> <p onClick={() => Setmenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src={nav_und} alt="" /> : <></>}</li>
-        <li> <AnchorLink className='anchor-link' offset={15} href='#portfolio'> <p onClick={() => Setmenu("portfolio")}>Portfolio</p></AnchorLink>{menu === "portfolio" ? <img src={nav_und} alt="" /> : <></>}</li>
-        <li> <AnchorLink className='anchor-link' offset={15} href='#contact'> <p onClick={() => Setmenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={nav_und} alt="" /> : <></>}</li>
-      </ul>
-      <AnchorLink className='anchor-link' offset={15} href='#contact'><div className="nav-connect">Connect With Me</div></AnchorLink>
-    </div>
-  )
-}
+      <AnchorLink className='anchor-link' offset={15} href='#about'>
+        <img onClick={() => Setmenu("about")} src={logo} alt="Logo" />
+      </AnchorLink>
 
-export default Navbar
+      {/* Hamburger icon */}
+      <div className='hamburger' onClick={toggleMenu}>
+        {mobileOpen ? <FaTimes size={24} color="#fff" /> : <FaBars size={24} color="#fff" />}
+      </div>
+
+      {/* Desktop & Mobile menu */}
+      <ul className={`nav-menu ${mobileOpen ? 'mobile-menu-open' : ''}`}>
+        {["home", "about", "services", "portfolio", "contact"].map((item) => (
+          <li key={item}>
+            <AnchorLink
+              className='anchor-link'
+              offset={15}
+              href={`#${item}`}
+              onClick={() => {
+                Setmenu(item);
+                closeMenu();
+              }}
+            >
+              <p>{item.charAt(0).toUpperCase() + item.slice(1)}</p>
+            </AnchorLink>
+            {menu === item ? <img src={nav_und} alt="" /> : null}
+          </li>
+        ))}
+      </ul>
+
+      <AnchorLink className='anchor-link' offset={15} href='#contact'>
+        <div className="nav-connect">Connect With Me</div>
+      </AnchorLink>
+    </div>
+  );
+};
+
+export default Navbar;
